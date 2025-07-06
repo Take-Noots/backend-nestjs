0fe1a00abfb27dc17b060106e2cec5387028e86a
desynkd <ravindupeeris180@gmail.com>
2025-07-06 18:13:06 +0530

    Print DB conn String

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  console.log(process.env.DB_CONN_STRING);
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser()); 
   await app.listen(process.env.PORT ?? 3000);
