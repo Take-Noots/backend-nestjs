8fe6db269675337ac1e25aa1a7eb7addf4475d3f
Sandun Sahiru <134186922+sandunsahiru@users.noreply.github.com>
2025-08-16 12:23:13 +0530

    Chat Module Update

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -4,15 +4,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { Chat, ChatSchema } from './chat.model';
+import { User, UserSchema } from '../user/user.model'; // Import User model
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Chat.name, schema: ChatSchema }
+      { name: Chat.name, schema: ChatSchema },
+      { name: User.name, schema: UserSchema } // Add User model reference for population
     ])
   ],
   controllers: [ChatController],
   providers: [ChatService],
-  exports: [ChatService], // Export service if other modules need it
+  exports: [ChatService],
 })
 export class ChatModule {}
